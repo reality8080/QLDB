634b123ccf250e4f68838ef1806de8786e7a7612
12a1n <12a1n@REALITY>
2024-07-26 19:43:40 +0700

    Add project files.

---
+++ Ban.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLiBan
+{
+    public class Ban
+    {
+        // Nhap thong tin ban
+        // Hien thi thong tin da nhap
+        // Mot cho quan li, mot cho nguoi dat
+        // The hien dat ban thanh cong
+    }
+}
