d20b9bb116d7a340ab9e038046aff916b8c785ce
Baocool_dev <baoproa6@gmail.com>
2024-07-27 23:19:40 +0700

    Thêm form đặt bàn

--- Form2.cs
+++ Form2.cs
@@ -16,5 +16,37 @@ namespace QuanLiBan
         {
             InitializeComponent();
         }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form3 form3 = new Form3();
+            form3.Show();
+            this.Hide();
+        }
     }
 }
